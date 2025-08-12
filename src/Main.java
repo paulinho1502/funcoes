c6b22323d17b21bba3123da8e6869744912713b2
FIC <fic@info.local>
2025-08-12 19:09:36 -0300

    mandando exercicios funcoes

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+// exercicio 1
+//    public static void mostrarMensagem (){
+//        System.out.println("Bem-vindo(a) ao programa");
+//    }
+
+
+//exercicio2
+//    public static void mostrarDobro(int num){
+//        int resultado = num * 2;
+//        System.out.println(resultado);
+//    }
+
+
+    //exercicio3
+//    public static int dividir(int num1, int num2){
+//      int resultado = num1  / num2;
+//      return resultado;
+//    }
+
+
+//     exercicio4
+//    public static boolean ehPar(int num1){
+//        boolean resultado = (num1 % 2 == 0);
+//        return resultado;
+//    }
+
+
+    public static double calcularMedia(double num1, double num2, double num3){
+
+    }
+
+
+
+    public static void main(String[] args) {
+//       (1)
+//        mostrarMensagem();
+
+//       (2)
+//        mostrarDobro(5);
+
+//       (3)
+//        int resultado = dividir(10, 2 );
+//
+//        System.out.println(resultado);
+
+//        (4)
+//        boolean resultado = ehPar(6);
+//        System.out.println(resultado);
+
+
+
+
+
+
+
+    }
+}
